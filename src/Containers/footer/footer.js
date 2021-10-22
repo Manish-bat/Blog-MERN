bb6a2248b6d344bed6959d8fbe2241f1b0a25bcf
Softuvo-Developer <manish-softuvo>
2021-10-22 09:10:16 +0530

    remove

---
+++ src/Containers/footer/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css'
+
+function footer() {
+
+    return (
+        <>
+            <footer className="fadeInUp col-md-12">
+                
+            </footer>
+           
+        </>
+    )
+}
+export default footer
